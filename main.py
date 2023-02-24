a6d60ce17be5f44b767e3c31bfb0b880b20c2f26
estemobs <57631804+Estemobs@users.noreply.github.com>
2023-02-24 16:07:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import json
 from discord.ext import commands
 
 # Import all intents from Discord API
@@ -8,16 +9,19 @@ intents = discord.Intents.all()
 # Create a bot with command prefix ";" and all imported intents
 bot = commands.Bot(command_prefix=";", intents=intents)
 
-# Dictionary to store all tags
-tags = {}
+
+# Get the absolute path of tags.json
+tags_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tags.json')
+
+# Load the tags from the JSON file
+with open(tags_path, 'r') as f:
+    tags = json.load(f)
+    if tags is None:
+        tags = {}
+
 
 @bot.event
 async def on_ready():
-    # Creates an empty tag.txt file when the bot starts
-    with open("tags.txt","w") as file:
-        file.write("")
-    # Prints the location of the tags.txt file in the console log
-    print(f"tags.txt created at {os.path.abspath('tags.txt')}")
     print("bot ready")
     # Changes the bot's playing status to ";help"
     await bot.change_presence(activity=discord.Game(name=";help"))
@@ -38,23 +42,24 @@ async def addtag(ctx, title: str):
     # Adds the title and content to the tags dictionary
     tags[title] = content.content
     await ctx.send('Tag created.')
-    # Writes the title and content to tags.txt file
-    with open("tags.txt", "a") as file:
-        file.write(f'{title}: {content.content}\n')
-#command to remove a tag
+    # Write the tags to the JSON file
+    with open(tags_path, 'w') as f:
+        json.dump(tags, f)
+
+# Command to remove a tag
 @bot.command()
 async def removetag(ctx, title: str):
     global tags
     if title in tags:
         #if the tag exists
         del tags[title] #remove the tag from the tags dictionary
-        with open("tags.txt", "w") as file:
-            for t,c in tags.items():
-                file.write(f'{t}: {c}\n') #rewrite the tags to the tags.txt file without the deleted tag
+        with open(tags_path, 'w') as f:
+            json.dump(tags, f) #write the tags to the JSON file without the deleted tag
         await ctx.send("Tag removed.") #confirm that the tag has been removed
     else:
         await ctx.send("Tag not found.") #if the tag does not exist, return an error message
 
+# Command to edit a tag
 @bot.command()
 async def tagedit(ctx, title: str):
     global tags
@@ -67,45 +72,53 @@ async def tagedit(ctx, title: str):
 
         content = await bot.wait_for('message', check=check)
         tags[title] = content.content
-        with open("tags.txt", "w") as file:
-            for t,c in tags.items():
-                file.write(f'{t}: {c}\n') #rewrite the tags to the tags.txt file with the edited tag
+        with open(tags_path, 'w') as f:
+            json.dump(tags, f) #write the tags to the JSON file with the edited tag
         await ctx.send("Tag edited.")
     else:
         await ctx.send("Tag not found.")
 
+# Command to rename a tag
 @bot.command()
 async def tagrename(ctx, old_title: str, new_title: str):
     global tags
     if old_title in tags:  # check if tag "old_title" exists
         tags[new_title] = tags[old_title]   # rename tag "old_title" to "new_title"
         del tags[old_title]    # delete the original tag "old_title"
-        with open("tags.txt", "w") as file:    # overwrite the tags.txt file with the renamed tag
-            for t,c in tags.items():
-                file.write(f'{t}: {c}\n') 
+        with open(tags_path, 'w') as f:    # overwrite the tags.json file with the renamed tag
+            json.dump(tags, f)
         await ctx.send("Tag renamed.")  # package the operation and send a message that the tag has been renamed
     else:
         await ctx.send("Tag not found.")  # if the original tag "old_title" is not found, send an error message
 
-
 # Command to display content of a tag given title
 @bot.command()
 async def tag(ctx, title: str):
-    global tags
-    # If title exists in dictionary tags then display it
+    with open(tags_path, 'r') as f:
+        tags = json.load(f)
     if title in tags:
         await ctx.send(tags[title])
     else:
-        await ctx.send("Tag not found.")
-# Command to list all tags
-@bot.command(pass_context=True)
+        await ctx.send(f"No tag found with the title '{title}'")
+
+@bot.command()
 async def taglist(ctx):
-    description = ""
-    for title, content in tags.items():
-        description += f"**{title}**: {content}\n" 
+    with open(tags_path, 'r') as f:
+        tags = json.load(f)
+        if tags is None:
+            tags = {}
+
+    tag_names = list(tags.keys())
+    description = "\n".join(tag_names)
     embed = discord.Embed(title="Tag List", description=description, color=discord.Color.green())
     embed.set_thumbnail(url="https://i.imgur.com/zV874EI.png")
     await ctx.send(embed=embed)
+
+
+@bot.command()
+async def server_list(ctx):
+    for guild in bot.guilds:
+        await ctx.send(f"Nom du serveur: {guild.name}")
 
 # Remove the built-in help command
 bot.remove_command("help")	
@@ -120,6 +133,7 @@ async def help(ctx):
     embed.add_field(name="tagrename", value="Modifie le nom du tag", inline=False)
     embed.add_field(name="tag", value="Affiche le contenu d'un tag avec un titre donné.", inline=False)
     embed.add_field(name="taglist", value="Affiche tous les tags dans une liste organisée.", inline=False)
+    embed.add_field(name="server_list", value="Voir la liste des serveurs sur lequel est le bot", inline=False)
     await ctx.send(embed=embed)
     
     bot.run('token')
